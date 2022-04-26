64de436dd9461400a612fec427b7559bd70dc5b4
thng17 <hangngtech@gmail.com>
2022-04-26 18:44:11 +0200

    added add-task component

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
