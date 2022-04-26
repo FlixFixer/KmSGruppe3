a4dc4bb77cb8532d802d6302b187ec6b35120bb5
swhemi <95661259+swhemi@users.noreply.github.com>
2022-04-26 18:43:56 +0200

    add components tasks-list & task-cards

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskCardComponent } from './task-card/task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksListComponent,
+    TaskCardComponent
   ],
   imports: [
     BrowserModule,
